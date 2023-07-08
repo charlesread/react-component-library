7219cd097fb8a26bd2c07209640f86fbe6a3784a
Charles Read <charles@charlesread.io>
2023-07-08 14:55:32 -0400

    init

---
+++ dist/index.mjs
@@ -0,0 +1,7 @@
+import t from "react";
+function r({ children: e }) {
+  return /* @__PURE__ */ t.createElement(t.Fragment, null, /* @__PURE__ */ t.createElement("button", null, e));
+}
+export {
+  r as Button
+};
